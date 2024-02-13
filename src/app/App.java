d62a1c1443d52827e84eadb64b278821af028c06
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-13 11:52:46 +0200

    add app and AppController

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.controller.AppController;
+
+public class App {
+    public static void main(String[] args) {
+        new AppController().Processing();
+    }
+}
---
+++ src/app/controller/AppController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import app.view.View;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AppController {
+    static Scanner scanner = new Scanner(System.in);
+
+    public void Processing() {
+
+    }
+}
